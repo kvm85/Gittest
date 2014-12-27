1f9dfcb8db25148489f09d2f68fa62956d7b185d
Volodia <Volodia@mycomp>
2014-12-27 19:09:08 +0200

    I have jast createt Java project

---
+++ Level15/src/Server.java
@@ -0,0 +1,41 @@
+import java.awt.HeadlessException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
+
+public class Server implements Runnable{
+	static private ServerSocket server;
+	static private Socket connection;
+	static private ObjectOutputStream output;
+	static private ObjectInputStream input;
+	
+		public void run() {
+			try{
+				server = new ServerSocket(5677,10);
+			while(true){
+				connection = server.accept();
+				output = new ObjectOutputStream(connection.getOutputStream());
+				input = new ObjectInputStream(connection.getInputStream());
+				output.writeObject("Ви надіслали:"+(String)input.readObject());	
+			  }
+			}catch(UnknownHostException e){
+			}catch(IOException e){	
+		    }catch(HeadlessException e){
+		    }catch(ClassNotFoundException e){}
+		 }
+		   
+		    }
+		
+
+	
+	
+	
+
+
